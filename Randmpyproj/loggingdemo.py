b8aa5a262bef60e6b40af0f49c0cdc0db2ae6106
imran-rabi <kidpalestine2023@gmail.com>
2025-11-06 13:35:27 -0600

    removed random files

--- Randmpyproj/loggingdemo.py
+++
@@ -1,9 +0,0 @@
-import logging as log
-
-log.basicConfig(level=log.INFO)
-
-log.info("this is a info message")
-log.warning("this is a warning message")
-log.error("this is a error message")
-log.critical("this is a critical message")
-log.debug("this is a debug message")
--- Randmpyproj/main.py
+++
@@ -1,3 +0,0 @@
-from hello import greet
-
-print(greet("Imran"))
